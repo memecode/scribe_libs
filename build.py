71f3af4ceb2c35ca7091cb1745b871d52495933a
Matthew Allen <fret@memecode.com>
2021-12-31 20:26:06 +1100

    Build system refactoring.

---
+++ build.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python3
+import os
+import sys
+import subprocess
+import shutil
+
+if os.name == "nt":
+    subfolders = ["build32", "build64"]
+    gen = ["Visual Studio 14 2015"]
+    gen.append(gen[0] + " Win64")
+    for n in range(len(subfolders)):
+        path = os.path.abspath(os.path.join(os.path.realpath(__file__), "..", subfolders[n]))
+        if os.path.exists(path):
+            shutil.rmtree(path)
+        os.mkdir(path)
+
+        print("Config:", path)
+        p = subprocess.run(["cmake", "-G", gen[n], ".."], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=path)
+        if p.returncode:
+            print("Error:", p.stdout.decode())
+            sys.exit(-1)
+        print("Build:", path)
+        p = subprocess.run(["cmake", "--build", ".", "--config", "Release"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=path)
+        if p.returncode:
+            print("Error:", p.stdout.decode())
+            sys.exit(-1)
+
+else:
+    print("Unsupported os:", os.name)
+    sys.exit(-1)
+
+
---
+++ common.cmake
@@ -0,0 +1,22 @@
+
+string(FIND ${CMAKE_GENERATOR} "Win64" HAS_WIN64)
+if (${HAS_WIN64} GREATER 0)
+    set(BITSIZE "64")
+else()
+    set(BITSIZE "32")
+endif()
+
+string(FIND ${CMAKE_GENERATOR} "2015" HAS_VS2015)
+if (${HAS_VS2015} GREATER 0)
+    set(VSVER "14")
+endif()
+
+string(FIND ${CMAKE_GENERATOR} "2017" HAS_VS2017)
+if (${HAS_VS2017} GREATER 0)
+    set(VSVER "15")
+endif()
+
+string(FIND ${CMAKE_GENERATOR} "2019" HAS_VS2019)
+if (${HAS_VS2019} GREATER 0)
+    set(VSVER "16")
+endif()
