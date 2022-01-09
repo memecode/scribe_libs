aa34d24bbb617dcc3712fbccda2d2c10268c94a3
Matthew Allen <fret@memecode.com>
2022-01-09 23:02:13 +1100

    Mac fixes

--- build.py
+++ build.py
@@ -8,25 +8,30 @@ if os.name == "nt":
     subfolders = ["build32", "build64"]
     gen = ["Visual Studio 14 2015"]
     gen.append(gen[0] + " Win64")
-    for n in range(len(subfolders)):
-        path = os.path.abspath(os.path.join(os.path.realpath(__file__), "..", subfolders[n]))
-        if os.path.exists(path):
-            shutil.rmtree(path)
-        os.mkdir(path)
 
-        print("Config:", path)
-        p = subprocess.run(["cmake", "-G", gen[n], ".."], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=path)
-        if p.returncode:
-            print("Error:", p.stdout.decode())
-            sys.exit(-1)
-        print("Build:", path)
-        p = subprocess.run(["cmake", "--build", ".", "--config", "Release"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=path)
-        if p.returncode:
-            print("Error:", p.stdout.decode())
-            sys.exit(-1)
+elif os.name == "posix":
+    
+    subfolders = ["build"]
+    gen = ["Xcode"]
 
 else:
     print("Unsupported os:", os.name)
     sys.exit(-1)
 
 
+for n in range(len(subfolders)):
+    path = os.path.abspath(os.path.join(os.path.realpath(__file__), "..", subfolders[n]))
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    os.mkdir(path)
+
+    print("Config:", path)
+    p = subprocess.run(["cmake", "-G", gen[n], ".."], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=path)
+    if p.returncode:
+        print("Error:", p.stdout.decode())
+        sys.exit(-1)
+    print("Build:", path)
+    p = subprocess.run(["cmake", "--build", ".", "--config", "Release"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=path)
+    if p.returncode:
+        print("Error:", p.stdout.decode())
+        sys.exit(-1)
