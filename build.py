5ceebbc0ee300a712ca0fdf4bcdd6fd4e0614b06
Matthew Allen <fret@memecode.com>
2023-08-19 10:31:08 +1000

    Make build.py multi/single config aware.

--- build.py
+++ build.py
@@ -7,10 +7,12 @@ import platform
 
 arch = []
 configs = ["Debug", "Release"]
+singleConfig = True
 if platform.system() == "Windows":
     subfolders = ["build-x64"]
     gen = ["Visual Studio 16 2019"]
     arch = ["-A", "x64"]
+    singleConfig = False
 elif platform.system() == "Darwin":    
     subfolders = ["build-x64", "build-arm64"]
     gen = ["Xcode", "Xcode"]
@@ -19,28 +21,45 @@ elif platform.system() == "Linux":
     gen = ["Unix Makefiles"]
 elif platform.system() == "Haiku":
     subfolders = ["build-x64"]
-    gen = ["Unix Makefiles"]
+    gen = ["Ninja"]
 else:
     print("Unsupported os:", os.name, platform.system())
     sys.exit(-1)
 
+first = True
 for n in range(len(subfolders)):
-    path = os.path.abspath(os.path.join(os.path.realpath(__file__), "..", subfolders[n]))
-    if os.path.exists(path):
-        shutil.rmtree(path)
-    os.mkdir(path)
-
-    print("Config:", path)
-    args = ["cmake", "-G", gen[n]] + arch + [".."]
-    print("args:", " ".join(args))
-    p = subprocess.run(args, cwd=path) # stdout=subprocess.PIPE, stderr=subprocess.STDOUT, 
-    if p.returncode:
-        print("Error: cmake failed.")
-        sys.exit(-1)
-
     for config in configs:
+        path = os.path.abspath(os.path.join(os.path.realpath(__file__), "..", subfolders[n]))
+        if singleConfig:
+            path = path + "-" + config.lower()
+        
+        if (first or singleConfig) and os.path.exists(path):
+            shutil.rmtree(path)
+        os.mkdir(path)
+
+        if first or singleConfig:
+            first = False
+            print("Configuring:", path)
+            args = ["cmake", "-G", gen[n]] + arch + [".."]
+            if singleConfig:
+                args += ["-DCMAKE_BUILD_TYPE="+config]
+
+            print("args:", " ".join(args))
+            p = subprocess.run(args, cwd=path) # stdout=subprocess.PIPE, stderr=subprocess.STDOUT, 
+            if p.returncode:
+                print("Error: cmake failed.")
+                sys.exit(-1)
+
         print(config, "Build:", path)
-        p = subprocess.run(["cmake", "--build", ".", "--config", config], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=path)
+        args = ["cmake", "--build", "."]
+        if not singleConfig:
+            args += ["--config", config]
+        if 0:
+            # Debug: don't hide output
+            print("args:", " ".join(args))
+            p = subprocess.run(args, cwd=path)
+        else:
+            p = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=path)
         if p.returncode:
             print(p.stdout.decode())
             print("Error: build failed.")
